03d2cc5257aa9f2fd63b50aab9360f3a8debf777
KaleChips22 <95776366+KaleChips22@users.noreply.github.com>
2024-10-08 15:08:46 -0400

    F: Navigation R: Rename variables

--- index.js
+++ index.js
@@ -11,14 +11,16 @@ function streamGenerator(button) {
     const totalNotes = length * division
     const noteWidth = Math.floor(12 / keys)
 
-    const sideLength = Math.floor(keys / 2)
+    const keysPerSide = keys / 2
     const isOdd = keys % 2 == 1
+
+    if (isOdd) keysPerSide -= 0.5
 
     var result = []
 
     for (let i = 0; i < totalNotes / 2; i++) {
-        const leftNoteLane = randomInt(1, sideLength)
-        const rightNoteLane = randomInt(1, sideLength) + 6
+        const leftNoteLane = randomInt(0, keysPerSide) * noteWidth
+        const rightNoteLane = randomInt(0, keysPerSide) * noteWidth + 6
         result.push({ width: noteWidth, lane: leftNoteLane })
         result.push({ width: noteWidth, lane: rightNoteLane })
     }
